5d2f819f1c5da6fb19752370edc5a9193f92a536
Shivank Kapoor <47286913+ShivankKapoor@users.noreply.github.com>
2026-06-12 02:28:42 -0500

    Add track service call

--- internal/handler/track_handler.go
+++ internal/handler/track_handler.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"visit-service/internal/dto"
+	"visit-service/internal/service"
 )
 
 type TrackHandler struct{}
@@ -18,15 +19,14 @@ func (h *TrackHandler) Track(w http.ResponseWriter, r *http.Request) {
 	slog.Info("Track request incoming")
 	var req dto.TrackRequest
 
-	if req.DeviceInfo == "" || req.PageVisited == "" {
-		http.Error(w, "deviceInfo and pageVisited are required", http.StatusBadRequest)
-		return
-	}
-
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		http.Error(w, "invalid request body", http.StatusBadRequest)
 		return
 	}
 
-	slog.Info("Track request parsed", "deviceInfo", req.DeviceInfo, "pageVisited", req.PageVisited)
+	if req.DeviceInfo == "" || req.PageVisited == "" {
+		http.Error(w, "deviceInfo and pageVisited are required", http.StatusBadRequest)
+		return
+	}
+	service.Track(req, r)
 }
--- internal/service/track-service.go
+++ internal/service/track-service.go
@@ -1,1 +1,12 @@
 package service
+
+import (
+	"log/slog"
+	"net/http"
+	"visit-service/internal/dto"
+)
+
+func Track(dto dto.TrackRequest, r *http.Request) {
+	var ip = GetClientIP(r)
+	slog.Info("Track request parsed", "deviceInfo", dto.DeviceInfo, "pageVisited", dto.PageVisited, "Ip", ip)
+}
